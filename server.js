41bd46eac7e5d1f7ca7d0d6741e1853a919fea37
Yixiu Jiang <yixiu.jiang@gmail.com>
2018-03-25 22:22:04 +1100

    add swagger

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
-const app = express()
+const port = 8080
+const exphbs  = require('express-handlebars');
+var swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./api.json');
+var app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+app.get('/about',(req, res)=>{
+  const content = 'This is about me'
+  res.render('about', {
+    content: content
+  });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.use(express.static('public'))
